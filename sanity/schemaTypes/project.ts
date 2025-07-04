19ec15762209ce888e8bd7c21819401803c1e531
Stephanie Thompson <steph@stephcodes.dev>
2025-07-03 22:45:08 -0500

    Created Sanity schemas for user and project

---
+++ sanity/schemaTypes/project.ts
@@ -0,0 +1,42 @@
+import {defineField, defineType} from "sanity";
+
+export const project = defineType ({
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title'
+      }
+    }),
+    defineField({
+      name: 'user',
+      type: 'reference',
+      to: { type: 'user' }
+    }),
+    defineField({
+      name: 'description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'category',
+      type: 'string',
+      validation: (Rule) => Rule.min(1).max(20).required().error("Please enter a category")
+    }),
+    defineField({
+      name: 'copy',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      type: 'url',
+    }),
+  ],
+})
