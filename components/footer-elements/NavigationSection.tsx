c102881ff5286abac2087baabaf54a6d24278a69
Stephanie Thompson <steph@stephcodes.dev>
2025-07-11 23:14:17 -0500

    Updated links in footer to match navbar

--- components/footer-elements/NavigationSection.tsx
+++ components/footer-elements/NavigationSection.tsx
@@ -7,23 +7,25 @@ import Link from "next/link";
 export const NavigationSection = () => {
   return (
     <nav className="flex gap-6 items-center flex-col md:flex-row self-stretch my-auto text-md md:text-2xl leading-none text-center text-white max-md:mt-10 max-md:max-w-full">
-      <Link href="/">
+      <Link href="/#hero">
         Home
       </Link>
-      <Link href="#my-work">
+      <Link href="/#my-work">
         My Work
       </Link>
-      <Link href="#experience">
+      <Link href="/#experience">
         Experience
       </Link>
-      <Link href="#about">
+      <Link href="/#about">
         About
       </Link>
-      <CustomButton
-            title="Hire Me"
-            btnType="button"
-            containerStyles="text-white rounded-sm bg-neutral-500 min-w-[130px] py-3"
-      />
+      <Link href="/#hire-me">
+        <CustomButton
+              title="Hire Me"
+              btnType="button"
+              containerStyles="text-white rounded-sm bg-neutral-500 min-w-[130px] py-3"
+        />
+      </Link>
     </nav>
   );
 };
